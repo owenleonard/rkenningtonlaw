1da84edab8398a8439946988c974caf76b4017a0
Owen Leonard <owenleonard@gmail.com>
2014-06-20 23:11:07 -0600

    make the two asides configurable

--- website/views.py
+++ website/views.py
@@ -7,21 +7,29 @@ def globalDisclaimer():
 	'''retrieves the global disclaimer to be placed at the bottom of the page'''
 	return Disclaimer.objects.filter(name="global").first()
 
+def spanish():
+	'''retrieves the spanish text to be placed on the right side of the page'''
+	return Content.objects.filter(title="spanish").first()
+
+def contactInfo():
+	'''retrieves the contact info to be placed on the right side of the page'''
+	return Content.objects.filter(title="contact").first()
+
 # Create your views here.
 def index(request):
 	content = Content.objects.filter(title="home").first()
-	return render_to_response('kenningtonlaw/index.html', {'disclaimer': globalDisclaimer(), 'content': content})
+	return render_to_response('kenningtonlaw/index.html', {'disclaimer': globalDisclaimer(), 'spanish': spanish(), 'contact':contactInfo(), 'content': content})
 
 def areas(request):
 	areas = PracticeArea.objects.filter(display=True)
-	return render_to_response('kenningtonlaw/areas.html', {'disclaimer': globalDisclaimer(), 'areas': areas})
+	return render_to_response('kenningtonlaw/areas.html', {'disclaimer': globalDisclaimer(), 'spanish': spanish(), 'contact':contactInfo(), 'areas': areas})
 
 def profile(request):
 	content = Content.objects.filter(title="profile").first()
-	return render_to_response('kenningtonlaw/profile.html', {'disclaimer': globalDisclaimer(), 'content': content})
+	return render_to_response('kenningtonlaw/profile.html', {'disclaimer': globalDisclaimer(), 'spanish': spanish(), 'contact':contactInfo(), 'content': content})
 
 def directions(request):
-	return render_to_response('kenningtonlaw/directions.html', {'disclaimer': globalDisclaimer()})
+	return render_to_response('kenningtonlaw/directions.html', {'disclaimer': globalDisclaimer(), 'spanish': spanish(), 'contact':contactInfo()})
 
 def contact(request):
 	if request.method == 'POST':
@@ -41,7 +49,7 @@ def contact(request):
 	else:
 		form = ContactForm()
 
-	return render(request, 'kenningtonlaw/contact.html', {'disclaimer': globalDisclaimer(), 'form': form})
+	return render(request, 'kenningtonlaw/contact.html', {'disclaimer': globalDisclaimer(), 'spanish': spanish(), 'contact':contactInfo(), 'form': form})
 
 def thanks(request):
-	return render_to_response('kenningtonlaw/thanks.html', {'disclaimer': globalDisclaimer()})
+	return render_to_response('kenningtonlaw/thanks.html', {'disclaimer': globalDisclaimer(), 'spanish': spanish(), 'contact':contactInfo()})
